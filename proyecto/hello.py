6642d942bd643c21b8b537f8eb2d517a39fa9da5
olimarteam <73653954+olimarteam@users.noreply.github.com>
2023-07-28 20:51:38 -0300

    cursando python

---
+++ proyecto/hello.py
@@ -0,0 +1,21 @@
+#hola mundo
+print("hola Python")
+print('hola mundo')
+ 
+"""
+este es un comentario en 
+varias
+varias
+lineas
+"""
+
+#tipo de datos
+print(type('Soy un string')) #str
+print(type(10)) #int
+print(type(10.5)) #float
+print(type(True)) #bool
+print(type(3+1j)) #complex
+
+
+my_string_variable = "Soy un string"
+print(my_string_variable)
